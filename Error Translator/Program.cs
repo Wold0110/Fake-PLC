1417b9ea842a05c0d955e09cfdf1b618fea29be6
Wold0110 <walter20020110@gmail.com>
2022-08-08 13:16:36 +0200

    the start

--- Error Translator/Program.cs
+++ Error Translator/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EasyModbus;
+//See https://aka.ms/new-console-template for more information
+List<Machine> machines = new List<Machine>();
+Console.WriteLine("Starting...");
+
+internal class Machine
+{
+    ModbusClient target;
+    int targetAddr;
+
+    ModbusClient source;
+    List<ErroCode> codes = new List<ErroCode>();
+
+    internal Machine(string targetIP, int targetPort, string sourceIP, int sourcePort, int targetAddr)
+    {
+        target = new(targetIP, targetPort);
+        source = new(sourceIP, sourcePort);
+        this.targetAddr = targetAddr;
+    }
+    internal bool AddCode(string line)
+    {
+
+    }
+}
+internal class ErroCode
+{
+    int addr;
+    int num;    
+} 
